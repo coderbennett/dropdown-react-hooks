0e7578e5ce3320c911990077e0e06153f102a8ec
joey bennett <joeykb17@gmail.com>
2022-10-24 14:27:21 -0700

    add conditional rendering depending on checkbox value

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,7 +2,9 @@ export default function Dropdown(prop) {
     
     return (
       <div className="Dropdown">
-        {`${prop.value}`}
+        {prop.value === true ? (
+          <>true that</>
+        ) : (<>false news</>)}
       </div>
     );
   }
